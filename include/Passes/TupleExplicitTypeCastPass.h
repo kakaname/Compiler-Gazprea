e8259dae587244bacccf59ff9753e86475958b8c
xiaofeiyu2019 <yufei24@ualberta.ca>
2022-11-14 23:36:23 -0700

    added canCastTo() for scalar types

--- include/Passes/TupleExplicitTypeCastPass.h
+++ include/Passes/TupleExplicitTypeCastPass.h
@@ -5,9 +5,21 @@
 #ifndef GAZPREABASE_TUPLEEXPLICITTYPECASTPASS_H
 #define GAZPREABASE_TUPLEEXPLICITTYPECASTPASS_H
 
+#include "VisitorPass.h"
+#include "PassManager.h"
+#include "Symbol/Symbol.h"
+#include "Passes/ExprTypeAnnotatorPass.h"
 
-class TupleExplicitTypeCastPass {
+struct TupleExplicitTypeCastPass: public VisitorPass<TupleExplicitTypeCastPass, void>  {
+    ASTPassManager *PM;
 
+    void visitExplicitCast(ExplicitCast *ExplicitCast);
+
+    void runOnAST(ASTPassManager &PManager, ASTNodeT *Root) {
+        assert(isa<Program>(Root) && "TupleExplicitTypeCastPass should run on the entire program");
+        PM = &PManager;
+        visit(Root);
+    }
 };
 
 
--- src/Passes/TupleExplicitTypeCastPass.cpp
+++ src/Passes/TupleExplicitTypeCastPass.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "Passes/TupleExplicitTypeCastPass.h"
+
+void TupleExplicitTypeCastPass::visitExplicitCast(ExplicitCast *ExplicitCast) {
+
+
+    // If it is not a tuple explicit casting, then we ignore it
+    if(!isa<TupleTy>(PM->getAnnotation<ExprTypeAnnotatorPass>(ExplicitCast->getExpr())))
+        return;
+
+    assert(isa<TupleTy>(ExplicitCast->TargetType) && "Explicit Type Casting was invalid");
+
+
+    auto ExprType = dyn_cast<TupleTy>(PM->getAnnotation<ExprTypeAnnotatorPass>(ExplicitCast->getExpr()));
+    auto TargetType = dyn_cast<TupleTy>(ExplicitCast->TargetType);
+
+    assert(ExprType->getNumOfMembers() == TargetType->getNumOfMembers() && "The size of tuples didn't match");
+
+}
