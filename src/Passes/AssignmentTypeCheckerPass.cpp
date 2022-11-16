7e9459df0d96292165a72ccb4a17f199ac61d943
Dhanrajbir Singh Hira <dhanrajb@ualberta.ca>
2022-11-15 20:19:06 -0700

    Fixed assignment type promotion leading to incorrect AST node usage.

--- src/Passes/AssignmentTypeCheckerPass.cpp
+++ src/Passes/AssignmentTypeCheckerPass.cpp
@@ -9,13 +9,15 @@ void AssignmentTypeCheckerPass::visitAssignment(Assignment *Assign) {
     auto IdentType = Assign->getIdentifier()->getIdentType();
     assert(IdentType && "Identifier must have their types assigned at this point.");
     auto AssignedType = PM->getAnnotation<ExprTypeAnnotatorPass>(Assign->getExpr());
-    assert((AssignedType->isSameTypeAs(IdentType)
-    || AssignedType->canPromoteTo(IdentType)) && "Incompatible type assigned");
 
+    // If they are already the same type, we don't care.
     if (AssignedType->isSameTypeAs(IdentType))
         return;
 
-    auto Cast =  PM->Builder.build<ExplicitCast>();
+    // If not, the expression must be promotable to the ident type.
+    assert(AssignedType->canPromoteTo(IdentType) && "Invalid assignment,"
+                                                    "types don't match");
+    auto Cast =  PM->Builder.build<TypeCast>();
     Cast->setTargetType(IdentType);
     Cast->setExpr(Assign->getExpr());
     Assign->setExpr(Cast);
@@ -26,13 +28,15 @@ void AssignmentTypeCheckerPass::visitDeclaration(Declaration *Decl) {
     auto IdentType = Decl->getIdentifier()->getIdentType();
     assert(IdentType && "Identifier must have their types assigned at this point.");
     auto AssignedType = PM->getAnnotation<ExprTypeAnnotatorPass>(Decl->getInitExpr());
-    assert((AssignedType->isSameTypeAs(IdentType)
-           || AssignedType->canPromoteTo(IdentType)) && "Incompatible type assigned");
 
+    // If they are already the same type, we don't care.
     if (AssignedType->isSameTypeAs(IdentType))
         return;
 
-    auto Cast =  PM->Builder.build<ExplicitCast>();
+    // If not, the expression must be promotable to the ident type.
+    assert(AssignedType->canPromoteTo(IdentType) && "Invalid assignment,"
+                                                    "types don't match");
+    auto Cast =  PM->Builder.build<TypeCast>();
     Cast->setTargetType(IdentType);
     Cast->setExpr(Decl->getInitExpr());
     Decl->setInitExpr(Cast);
