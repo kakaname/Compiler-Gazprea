6d4bbc34e12351221d53f6d51ec7d2e822a984cc
Rajan Maghera <rmaghera@ualberta.ca>
2022-12-05 06:58:02 -0700

    Refactor runtime

---
+++ runtime/src/runtime.h
@@ -0,0 +1,203 @@
+//
+// Created by rajan on 12/01/22.
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define VECTOR_OP(enum_name, op) \
+    case enum_name:                  \
+        res_data[i] = a_data[i] op b_data[i]; \
+        break;
+#define ARITHMETIC_OPS \
+switch (op) {          \
+    VECTOR_OP(MUL, *)  \
+    VECTOR_OP(DIV, /)  \
+    VECTOR_OP(ADD, +)  \
+    VECTOR_OP(SUB, -)  \
+    default:           \
+        exit(1);       \
+}
+#define VECTOR_A_INIT(type) \
+    type *a_data = (type *) a->data;
+#define VECTOR_RES_INIT(type) \
+    type *res_data = (type *) res.data;
+#define VECTOR_DATA_INIT_SINGLE(type) \
+    VECTOR_A_INIT(type) \
+    VECTOR_RES_INIT(type)
+#define VECTOR_DATA_INIT(type) \
+    VECTOR_A_INIT(type)        \
+    type *b_data = (type *) b->data;
+#define VECTOR_DATA_INIT_RES(type) \
+    VECTOR_DATA_INIT(type)         \
+    type *res_data = (type *) res.data;
+#define VECTOR_COMPARISON \
+switch (op) {             \
+VECTOR_OP(LT, <)          \
+VECTOR_OP(GT, >)          \
+VECTOR_OP(LTEQ, <=)       \
+VECTOR_OP(GTEQ, >=)       \
+default:            \
+exit(1);                  \
+}
+#define VECTOR_EQ_OP(enum_name, opposite_op) \
+    case enum_name:                          \
+        if (a_data[i] opposite_op b_data[i]) \
+            return 0;                         \
+        break;
+#define EQUALITY_OPS \
+switch (op) {          \
+    VECTOR_EQ_OP(EQ, !=)  \
+    VECTOR_EQ_OP(NEQ, ==) \
+    default:           \
+        exit(1);       \
+}
+#define VECTOR_CONCAT \
+for (int i = 0; i < a->size; i++) { \
+res_data[i] = a_data[i]; \
+} \
+for (int i = 0; i < b->size; i++) { \
+res_data[i + a->size] = b_data[i]; \
+}
+#define VECTOR_DOTPRODUCT \
+for (int i = 0; i < a->size; i++) { \
+res += a_data[i] * b_data[i]; \
+}
+#define VECTOR_BY \
+for (u_int32_t i = 0; i < res.size; i++) { \
+res_data[i] = a_data[i*stride];            \
+}
+#define VECTOR_ACCESS(type) \
+type rt_vector_access_##type(struct vector *v, u_int32_t idx, u_int32_t unchecked) { \
+                            \
+    if (idx >= v->size) {   \
+        if (unchecked) {  \
+            return 0; \
+        } else {            \
+            exit(1); \
+        } \
+    }                       \
+    u_int32_t real_idx = idx;                                                        \
+    if (v->idx != 0) { \
+        real_idx = v->idx[idx];\
+    }                       \
+    return ((type *) v->data)[real_idx];                        \
+}
+#define VECTOR_SET(type) \
+void rt_vector_set_##type(struct vector *v, u_int32_t idx, type val, u_int32_t unchecked) { \
+    if (idx >= v->size) {\
+        if (unchecked) {  \
+            return; \
+        } else {         \
+            exit(1); \
+        } \
+    }                    \
+    u_int32_t real_idx = idx; \
+    if (v->idx != 0) { \
+        real_idx = v->idx[idx];\
+    }                    \
+                         \
+    printf("%d", val);                     \
+    ((type *) v->data)[real_idx] = val;                        \
+}
+#define MATRIX_ACCESS(type) \
+type rt_matrix_access_##type(struct matrix *m, u_int32_t row, u_int32_t col, u_int32_t unchecked) { \
+    if (row >= m->rows || col >= m->cols) { \
+        if (unchecked) { \
+            return 0; \
+        } else { \
+            exit(1); \
+        } \
+    }                       \
+                            \
+    u_int32_t row_idx = row;\
+    if (m->idx != 0) { \
+        row_idx = m->idx[row]; \
+    } \
+    struct vector *v = &m->data[row]; \
+    return rt_vector_access_##type(v, col, unchecked);                    \
+}
+#define MATRIX_SET(type) \
+void rt_matrix_set_##type(struct matrix *m, u_int32_t row, u_int32_t col, type val, u_int32_t unchecked) { \
+    if (row >= m->rows || col >= m->cols) { \
+        if (unchecked) { \
+            return; \
+        } else { \
+            exit(1); \
+        } \
+    }                    \
+ \
+    u_int32_t row_idx = row;\
+    if (m->idx != 0) { \
+        row_idx = m->idx[row]; \
+    } \
+    struct vector *v = &m->data[row]; \
+    rt_vector_set_##type(v, col, val, unchecked);                    \
+}
+struct stream_store {
+    size_t front; // Front of queue
+    size_t back;  // Back of queue
+    size_t curr;  // Current position when "peeking"
+    size_t eof;
+    char stream_state;
+    char buffer[1025];
+};
+
+enum vector_type {
+    VECTOR_TYPE_BOOL = 0,
+    VECTOR_TYPE_CHAR = 1,
+    VECTOR_TYPE_INT = 2,
+    VECTOR_TYPE_FLOAT = 3
+};
+
+enum vector_arithmeic_op_kind {
+    EXP = 0,
+    MUL,
+    DIV,
+    MOD,
+    ADD,
+    SUB
+};
+enum vector_comparison_op_kind {
+    LT = 0, GT, LTEQ, GTEQ
+};
+enum vector_logical_op_kind {
+    AND = 0, OR, XOR, EQ, NEQ
+};
+
+struct vector {
+    u_int32_t size;
+    u_int32_t *idx;
+    u_int32_t type;
+    void *data;
+};
+
+struct matrix {
+
+    u_int32_t rows;
+    u_int32_t cols;
+    u_int32_t *idx;
+    u_int32_t type;
+    struct vector *data;
+};
+
+
+struct vector rt_vector_not(struct vector *v);
+struct vector rt_vector_sub(struct vector *v);
+struct vector rt_vector_arith(struct vector *a, struct vector *b, u_int32_t op);
+struct vector rt_vector_comp(struct vector *a, struct vector *b, u_int32_t op);
+struct vector rt_vector_logical(struct vector *a, struct vector *b, u_int32_t op);
+u_int8_t rt_vector_eq(struct vector *a, struct vector *b, u_int32_t op);
+char rt_vector_access_char(struct vector *v, u_int32_t idx, u_int32_t unchecked);
+int rt_vector_access_int(struct vector *v, u_int32_t idx, u_int32_t unchecked);
+float rt_vector_access_float(struct vector *v, u_int32_t idx, u_int32_t unchecked);
+void rt_vector_set_char(struct vector *v, u_int32_t idx, char val, u_int32_t unchecked);
+void rt_vector_set_int(struct vector *v, u_int32_t idx, int val, u_int32_t unchecked);
+void rt_vector_set_float(struct vector *v, u_int32_t idx, float val, u_int32_t unchecked);
+struct vector rt_vector_create_deep_copy(struct vector *v);
+struct vector rt_vector_view_scalar(struct vector *v, u_int32_t idx);
+struct vector rt_vector_view_vector(struct vector *v, struct vector *idx);
---
+++ runtime/src/vectors.c
@@ -0,0 +1,298 @@
+//
+// Created by rajan on 12/01/22.
+//
+
+#include "runtime.h"
+
+struct vector rt_vector_new(enum vector_type type, u_int32_t size) {
+    struct vector v;
+    v.size = size;
+    v.idx = 0;
+    v.type = type;
+    switch (type) {
+        case VECTOR_TYPE_CHAR:
+            // NOTE: booleans are stored as bytes
+            v.data = malloc(sizeof(char) * size);
+            break;
+        case VECTOR_TYPE_INT:
+            v.data = malloc(sizeof(int) * size);
+            break;
+        case VECTOR_TYPE_FLOAT:
+            v.data = malloc(sizeof(float) * size);
+            break;
+        default:
+            exit(1);
+    }
+    return v;
+}
+
+
+// TODO fix mod and power
+
+struct vector rt_vector_not(struct vector *a) {
+    struct vector res = rt_vector_new(a->type, a->size);
+    VECTOR_DATA_INIT_SINGLE(char)
+    for (int i = 0; i < a->size; i++) {
+        res_data[i] = !a_data[i];
+    }
+    return res;
+}
+
+struct vector rt_vector_sub(struct vector *a) {
+    struct vector res = rt_vector_new(a->type, a->size);
+    if (a->type == VECTOR_TYPE_INT) {
+        VECTOR_DATA_INIT_SINGLE(int)
+        for (int i = 0; i < a->size; i++) {
+            res_data[i] = -a_data[i];
+        }
+    } else if (a->type == VECTOR_TYPE_FLOAT) {
+        VECTOR_DATA_INIT_SINGLE(float)
+        for (int i = 0; i < a->size; i++) {
+            res_data[i] = -a_data[i];
+        }
+    } else {
+        exit(1);
+    }
+    return res;
+}
+
+struct vector rt_vector_arith(struct vector *a, struct vector *b, u_int32_t op) {
+    // At this point, the vectors should be of the same type (innerty and size)
+    // As well, only vectors of real and float are supported
+
+    struct vector res = rt_vector_new(a->type, a->size);
+
+
+    if (res.type == VECTOR_TYPE_INT) {
+        VECTOR_DATA_INIT_RES(int)
+        for (int i = 0; i < res.size; i++) {
+            ARITHMETIC_OPS
+        }
+
+    } else if (res.type == VECTOR_TYPE_FLOAT) {
+        VECTOR_DATA_INIT_RES(float)
+        for (int i = 0; i < res.size; i++) {
+            ARITHMETIC_OPS
+        }
+    } else {
+        exit(1);
+    }
+
+    return res;
+}
+
+
+struct vector rt_vector_comp(struct vector *a, struct vector *b, u_int32_t op) {
+    // At this point, the vectors should be of the same type (innerty and size)
+    // As well, only vectors of real and float are supported
+
+    struct vector res = rt_vector_new(VECTOR_TYPE_BOOL, a->size);
+
+    if (a->type == VECTOR_TYPE_INT) {
+        VECTOR_RES_INIT(char)
+        VECTOR_DATA_INIT(int)
+        for (int i = 0; i < res.size; i++) {
+            VECTOR_COMPARISON
+        }
+
+    } else if (b->type == VECTOR_TYPE_FLOAT) {
+        VECTOR_RES_INIT(char)
+        VECTOR_DATA_INIT(float)
+        for (int i = 0; i < res.size; i++) {
+            VECTOR_COMPARISON
+        }
+    } else {
+        exit(1);
+    }
+
+    return res;
+}
+
+struct vector rt_vector_logical(struct vector *a, struct vector *b, u_int32_t op) {
+    // At this point, the vectors should be of the same type (innerty and size)
+    // As well, only vectors of real and float are supported
+
+    struct vector res = rt_vector_new(a->type, a->size);
+
+    if (res.type == VECTOR_TYPE_BOOL) {
+        VECTOR_DATA_INIT_RES(char)
+        for (int i = 0; i < res.size; i++) {
+            switch (op) {
+                case AND:
+                    res_data[i] = a_data[i] && b_data[i];
+                    break;
+                case OR:
+                    res_data[i] = a_data[i] || b_data[i];
+                    break;
+                case XOR:
+                    res_data[i] = a_data[i] ^ b_data[i];
+                    break;
+                default:
+                    exit(1);
+            }
+        }
+    } else {
+        exit(1);
+    }
+    return res;
+}
+
+u_int8_t rt_vector_eq(struct vector *a, struct vector *b, u_int32_t op) {
+
+    for (int i = 0; i < a->size; i++) {
+        if (a->type == VECTOR_TYPE_INT) {
+            VECTOR_DATA_INIT(int)
+            EQUALITY_OPS
+        } else if (a->type == VECTOR_TYPE_FLOAT) {
+            VECTOR_DATA_INIT(float)
+            EQUALITY_OPS
+        } else if (a->type == VECTOR_TYPE_BOOL || a->type == VECTOR_TYPE_CHAR) {
+            VECTOR_DATA_INIT(char)
+            EQUALITY_OPS
+            exit(1);
+        }
+    }
+
+    return 1;
+}
+
+struct vector rt_vector_concat(struct vector *a, struct vector *b) {
+
+    struct vector res = rt_vector_new(a->type, a->size + b->size);
+
+    if (res.type == VECTOR_TYPE_INT) {
+        VECTOR_DATA_INIT_RES(int)
+        VECTOR_CONCAT
+    } else if (res.type == VECTOR_TYPE_FLOAT) {
+        VECTOR_DATA_INIT_RES(float)
+        VECTOR_CONCAT
+    } else if (res.type == VECTOR_TYPE_BOOL || res.type == VECTOR_TYPE_CHAR) {
+        VECTOR_DATA_INIT_RES(char)
+        VECTOR_CONCAT
+    } else {
+        exit(1);
+    }
+
+    return res;
+
+}
+
+u_int32_t rt_vector_dotproduct_int(struct vector *a, struct vector *b) {
+    u_int32_t res = 0;
+    VECTOR_DATA_INIT(int)
+    VECTOR_DOTPRODUCT
+    return res;
+}
+
+float rt_vector_dotproduct_real(struct vector *a, struct vector *b) {
+    float res = 0;
+    VECTOR_DATA_INIT(float)
+    VECTOR_DOTPRODUCT
+    return res;
+}
+
+struct vector rt_vector_by(struct vector *a, u_int32_t stride) {
+    struct vector res = rt_vector_new(a->type, (a->size + stride - 1) / stride);
+
+    if (res.type == VECTOR_TYPE_INT) {
+        VECTOR_DATA_INIT_SINGLE(int)
+        VECTOR_BY
+    } else if (res.type == VECTOR_TYPE_FLOAT) {
+        VECTOR_DATA_INIT_SINGLE(float)
+        VECTOR_BY
+    } else if (res.type == VECTOR_TYPE_BOOL || res.type == VECTOR_TYPE_CHAR) {
+        VECTOR_DATA_INIT_SINGLE(char)
+        VECTOR_BY
+    } else {
+        exit(1);
+    }
+
+    return res;
+
+}
+
+VECTOR_ACCESS(char)
+
+VECTOR_ACCESS(int)
+
+VECTOR_ACCESS(float)
+
+
+
+struct vector rt_vector_create_deep_copy(struct vector *v) {
+    struct vector newV;
+    newV.size = v->size;
+    newV.idx = 0;
+    newV.type = v->type;
+
+    if (v->idx == 0) {
+        switch (v->type) {
+            case VECTOR_TYPE_BOOL:
+            case VECTOR_TYPE_CHAR:
+                newV.data = malloc(sizeof(char) * v->size);
+                memcpy(newV.data, v->data, sizeof(char) * v->size);
+                break;
+            case VECTOR_TYPE_INT:
+                newV.data = malloc(sizeof(int) * v->size);
+                memcpy(newV.data, v->data, sizeof(int) * v->size);
+                break;
+            case VECTOR_TYPE_FLOAT:
+                newV.data = malloc(sizeof(float) * v->size);
+                memcpy(newV.data, v->data, sizeof(float) * v->size);
+                break;
+        }
+    } else {
+        switch (v->type) {
+            case VECTOR_TYPE_BOOL:
+            case VECTOR_TYPE_CHAR:
+                newV.data = malloc(sizeof(char) * v->size);
+                for (int i = 0; i < v->size; i++) {
+                    ((char *) newV.data)[i] = rt_vector_access_char(v, i, 0);
+                }
+                break;
+            case VECTOR_TYPE_INT:
+                newV.data = malloc(sizeof(int) * v->size);
+                for (int i = 0; i < v->size; i++) {
+                    ((int *) newV.data)[i] = rt_vector_access_int(v, i, 0);
+                }
+                break;
+            case VECTOR_TYPE_FLOAT:
+                newV.data = malloc(sizeof(float) * v->size);
+                for (int i = 0; i < v->size; i++) {
+                    ((float *) newV.data)[i] = rt_vector_access_float(v, i, 0);
+                }
+                break;
+        }
+    }
+    return newV;
+}
+
+VECTOR_SET(char)
+
+VECTOR_SET(int)
+
+VECTOR_SET(float)
+
+
+struct vector rt_vector_view_scalar(struct vector *v, u_int32_t idx) {
+    struct vector newV;
+
+    newV.size = 1;
+    newV.idx = malloc(sizeof(u_int32_t));
+    newV.idx[0] = idx;
+    newV.type = v->type;
+    newV.data = v->data;
+    return newV;
+}
+
+struct vector rt_vector_view_vector(struct vector *v, struct vector *idx) {
+    struct vector newV;
+
+    // TODO confirm this works with the test case
+    newV.size = idx->size;
+    newV.idx = idx->data;
+    newV.type = v->type;
+    newV.data = v->data;
+    return newV;
+}
+
