dfe6df99617a4b5a522bc31651f552a38465ca78
Rajan Maghera <rmaghera@ualberta.ca>
2022-12-08 13:48:08 -0700

    Fix incorrect pass order
    Co-Authored-By: Youngwoo Seo <63736151+kakaname@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -86,7 +86,6 @@ int main(int argc, char **argv) {
     Manager.setResource<SubExpressionCacheSet>(
         SubExpressionCacheSet());
 
-    Manager.registerPass(ExprTypeAnnotatorPass());
     Manager.registerPass(ScopeResolutionPass());
 //    Manager.registerAnonymousPass(ASTPrinterPass());
     Manager.registerPass(ExprTypeAnnotatorPass());
